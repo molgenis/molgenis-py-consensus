946f701c58be3820b05cc101c425c6903925b0f6
mkslofstra <mariska-karina@hotmail.com>
2019-06-28 15:39:25 +0200

    fix(main): Update main to make pipeline executable again

--- consensus/__main__.py
+++ consensus/__main__.py
@@ -5,11 +5,12 @@ from consensus.MolgenisConfigParser import MolgenisConfigParser as ConfigParser
 from consensus.HistorySorter import HistorySorter
 from consensus.ConsensusReporter import ConsensusReporter
 from consensus.ConsensusFileGenerator import ConsensusFileGenerator
+from consensus.ConsensusTableUploader import ConsensusTableUploader
 
 
 def main():
     # Get data from config
-    config = ConfigParser('config/config.txt')
+    config = ConfigParser('../config/config.txt')
     consensus_table = config.prefix + config.consensus
     comments_table = config.prefix + config.comments
     molgenis_server = molgenis.Session(config.server)
@@ -20,11 +21,12 @@ def main():
     molgenis_server.login(config.username, config.password)
 
     # Retrieve data
-    data = DataRetriever(config.labs, config.prefix, molgenis_server, history_table)
-    lab_data = data.all_lab_data
+    retriever = DataRetriever(config.labs, config.prefix, molgenis_server, history_table)
+    retriever.retrieve_all_data()
+    lab_data = retriever.all_lab_data
 
     # Sort history on export
-    history = data.history
+    history = retriever.history
     sorted_history = HistorySorter(history, previous_exports).sorted_history
 
     # Generate consensus table in memory
@@ -33,16 +35,20 @@ def main():
     lab_classifications = consensus_generator.all_lab_classifications
 
     # Generate and upload CSV with consensus table
-    ConsensusFileGenerator(
+    file_generator = ConsensusFileGenerator(
         data={'consensus_data': consensus, 'lab_classifications': lab_classifications, 'history': sorted_history},
-        molgenis_server=molgenis_server,
         tables={'consensus_table': consensus_table, 'comments_table': comments_table})
+    consensus_file_name, comments_file_name = file_generator.generate_consensus_files()
+
+    # Upload files
+    uploader = ConsensusTableUploader(molgenis_server, consensus_file_name, comments_file_name)
+    uploader.update_consensus()
 
     # Generate reports
     prefix = config.prefix
     csv = prefix + 'consensus.csv'
     public = prefix + 'public_consensus'
-    ConsensusReporter(csv, molgenis_server, config.labs, public, config.prefix).process_consensus()
+    ConsensusReporter(csv, molgenis_server, config.labs, public, prefix).process_consensus()
 
     molgenis_server.logout()
 
