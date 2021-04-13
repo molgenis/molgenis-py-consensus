bd990705cf84f6fe1ce47438fde3874c3d7d9e8e
mkslofstra <mariska-karina@hotmail.com>
2021-04-13 09:11:10 +0200

    improvement: remove molgenis client from setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='consensus',
     version='1.0',
     packages=find_packages(),
-    install_requires=['molgenis-py-client>=2.1.0', 'progressbar2==3.39.3', 'termcolor==1.1.0', 'yaspin==0.14.3', 'pandas'],
+    install_requires=['progressbar2==3.39.3', 'termcolor==1.1.0', 'yaspin==0.14.3', 'pandas'],
     author='Mariska Slofstra',
     license='GNU Lesser General Public License 3.0',
     test_suite='nose.collector',
