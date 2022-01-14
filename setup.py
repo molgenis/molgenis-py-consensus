98ece0dd9ad15fc41fc40fa5390120c6795419e2
mkslofstra <mariska-karina@hotmail.com>
2022-01-14 11:07:41 +0100

    ci: change cython

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='2.0',
     packages=find_packages(),
     install_requires=['progressbar2==3.39.3', 'termcolor==1.1.0', 'yaspin==0.14.3', 'pandas==1.2.3',
-                      'numpy==1.21.5', 'cython==0.29.26'],
+                      'cython', 'numpy==1.21.5'],
     python_requires='>=3.7.1',
     author='Mariska Slofstra',
     license='GNU Lesser General Public License 3.0',
